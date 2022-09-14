90576bfb521a569f2c67a9b0743e563db5bbe2f3
Yasser Jimenez <yasserjimenezmartinez@hotmail.com>
2022-09-13 19:24:31 -0500

    backend setup

---
+++ pages/login.js
@@ -0,0 +1,40 @@
+
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function LoginPage() {
+
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name] : e.target.value
+        });
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(credentials);
+        const response = await axios.post('/api/auth/login');
+        console.log(response);
+    }
+
+    return (
+        <>
+            <form onSubmit={ handleSubmit } >
+                <input name="email" type={"email"} placeholder={"email"}
+                    onChange={ handleChange }
+                />
+                <input name="password" type={"password"} placeholder={"password"}
+                    onChange={ handleChange }
+                />
+                <button>Login</button>
+            </form>
+        </>
+    );
+}
